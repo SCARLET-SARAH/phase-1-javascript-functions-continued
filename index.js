892f29c0d26395fa0a0e1ab909a8c37a8ba878c8
SCARLET-SARAH <162133775+SCARLET-SARAH@users.noreply.github.com>
2024-03-19 21:38:06 +0300

    create functions.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+function razzle() {
+    console.log("You've been razzled!");
+  }
+  function razzle() {
+    console.log("You've been razzled!");
+  }
+  razzle();
+  //=> "You've been razzled!"
+  razzle(); //=> "You've been razzled!"
+function razzle() {
+  console.log("You've been razzled!");
+}
+function razzle(lawyer = "Billy", target = "'em") {
+    console.log(`${lawyer} razzle-dazzles ${target}!`);
+  }
+  console.log("Yet more razzling");
+  razzle(); //=> Billy razzle-dazzles 'em!
+  razzle("Methuselah", "T'challah"); //=> Methuselah razzle-dazzles T'challah!
+  1 + 1; //=> 2
+  "Razzle " + "dazzle!"; //=> "Razzle dazzle!"
+  console.log("Yet more razzling");
+  
+  const fn = function () {
+    console.log("Yet more razzling");
+  }; //=> undefined
+  fn; //=> ƒ () { console.log("Yet more razzling") }
+  fn(); // "Yet more razzling"
+
+  function outer(greeting, msg = "It's a fine day to learn") {
+    const innerFunction = function (name, lang = "Python") {
+      return `${greeting}, ${name}! ${msg} ${lang}`;
+    };
+    return innerFunction;
+  }
